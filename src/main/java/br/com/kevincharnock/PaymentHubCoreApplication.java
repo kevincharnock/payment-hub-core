30ecda762ebdd39fb546001783bca2264232b38e
Kevin Charnock <kevinshuldiner@gmail.com>
2026-01-09 11:10:08 -0300

    feat: Estrutura inicial

---
+++ src/main/java/br/com/kevincharnock/PaymentHubCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.kevincharnock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentHubCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentHubCoreApplication.class, args);
+	}
+
+}
